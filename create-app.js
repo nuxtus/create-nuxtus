2905c04b7eb053d5a7101934a75726e1883bcc23
Craig Harman <324026+craigharman@users.noreply.github.com>
2022-07-04 17:51:26 +0800

    Fix minor issue with error testing

--- create-app.js
+++ create-app.js
@@ -32,32 +32,33 @@ const startDirectus = function () {
 const autoCollections = function () {
 	const hookSpinner = new Spinner("%s Installing Nuxtus hook...").start()
 
-	execSync(
-		"cd server && npm install @nuxtus/directus-extension-nuxtus-hook --save-dev",
-		{
-			stdio: "ignore",
-		}
-	)
+	try {
+		execSync(
+			"cd server && npm install @nuxtus/directus-extension-nuxtus-hook --save-dev",
+			{
+				stdio: "ignore",
+			}
+		)
 
-	const source = path.join(
-		"server",
-		"node_modules",
-		"@nuxtus",
-		"directus-extension-nuxtus-hook",
-		"dist",
-		"index.js"
-	)
-	const subDest = path.join(
-		"server",
-		"extensions",
-		"hooks",
-		"directus-extension-nuxtus-hook"
-	)
-	fs.mkdirSync(subDest)
-	const dest = path.join(subDest, "index.js")
-	fs.copyFileSync(source, dest)
-	hookSpinner.stop(true)
-	if (error) {
+		const source = path.join(
+			"server",
+			"node_modules",
+			"@nuxtus",
+			"directus-extension-nuxtus-hook",
+			"dist",
+			"index.js"
+		)
+		const subDest = path.join(
+			"server",
+			"extensions",
+			"hooks",
+			"directus-extension-nuxtus-hook"
+		)
+		fs.mkdirSync(subDest)
+		const dest = path.join(subDest, "index.js")
+		fs.copyFileSync(source, dest)
+		hookSpinner.stop(true)
+	} catch (err) {
 		console.error(chalk.red(`Failed installing Nuxtus hook: ${error}`))
 		return
 	}
